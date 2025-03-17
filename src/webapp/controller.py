271d3f1131fad775eaf63ef99f2b2668aa3836cc
David Bian <davidto1122@gmail.com>
2025-03-17 16:29:26 -0400

    Add Review view model
    Add view loader logic

--- src/webapp/controller.py
+++ src/webapp/controller.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
+from view_loading import view_loader
+
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('index.html', person=name)
+    example_review = view_loader.load(name)
+    return render_template('index.html', review=example_review)
---
+++ src/webapp/view_loading/view_loader.py
@@ -0,0 +1,5 @@
+from view_model.review import Review
+
+def load(name: str):
+
+    return Review(name)
