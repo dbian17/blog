c98a2630bf789b738e30f95248c3a25b2e4185a1
David Bian <davidto1122@gmail.com>
2025-03-21 19:41:17 +0900

    Add page for review form submission

--- src/webapp/controller.py
+++ src/webapp/controller.py
@@ -8,13 +8,17 @@ from service_client.dynamo import dynamo_service_client
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def load_page():
     places = dynamo_service_client.get_places()
     return render_template('index.html', places=places)
 
+@app.route('/review', methods=['GET'])
+def load_review_submission():
+    return render_template('review_form.html')
+
 @app.route('/place', methods=['POST'])
 def add_place():
-    place = request_parser.parse_place(request.data)
+    place = request_parser.parse_place(request)
     dynamo_service_client.add_place(place)
     return "receive"
--- src/webapp/view_loading/request_parser.py
+++ src/webapp/view_loading/request_parser.py
@@ -1,7 +1,14 @@
+from flask import request
 from view_model.place import Place
 import simplejson as json
 
 # Parse request body into place object
-def parse_place(request_body: str):
-    place_dict = json.loads(request_body)
-    return Place(**place_dict)
+# 1. Check if parameters in body
+# 2. Check if parameters in form
+def parse_place(request: request):
+    if request.form:
+        place = Place(**request.form.to_dict())
+    else:
+        place_dict = json.loads(request.data)
+        place = Place(**place_dict)
+    return place
