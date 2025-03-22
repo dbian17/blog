6d2f5002975e75a5cb9a26e55acad6169cf9ef9c
David Bian <davidto1122@gmail.com>
2025-03-22 14:00:59 +0900

    Add tagline field to Place

--- src/webapp/view_model/place.py
+++ src/webapp/view_model/place.py
@@ -3,11 +3,12 @@ from decimal import Decimal
 
 class Place:
 
-    def __init__(self, name: str, rating: float, types: list[str], review: str):
+    def __init__(self, name: str, rating: float, types: list[str], review: str, tagline: str = None):
         self.name = name.lower().replace(" ", "-")
         self.rating = Decimal(str(rating))
         self.types = types
         self.review = review
+        self.tagline = tagline
 
     def __str__(self):
         return json.dumps(self.__dict__)
