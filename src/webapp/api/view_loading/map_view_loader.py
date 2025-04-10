8d8964690ba90e29594591fda09ef820bdecff1f
David Bian <davidto1122@gmail.com>
2025-04-09 23:12:25 -0400

    Create multiple pins for place if multiple coordinates present

--- src/webapp/api/view_loading/map_view_loader.py
+++ src/webapp/api/view_loading/map_view_loader.py
@@ -2,10 +2,18 @@ from api.view_model.place import Place
 import simplejson as json
 
 def create_map_pin_dict(place: Place):
-    map_pin = {}
-    map_pin['name'] = place.name
-    map_pin['coordinates'] = place.coordinates
-    return map_pin
+    place_map_pins = []
+    if place.coordinates:
+        for i in range(len(place.coordinates)//2):
+            map_pin = {}
+            map_pin['name'] = place.name
+            map_pin['rating'] = place.rating
+            map_pin['coordinates'] = place.coordinates[i*2: i*2+2]
+            place_map_pins.append(map_pin)
+    return place_map_pins
 
 def load(places: list[Place]):
-    return [create_map_pin_dict(place) for place in places]
+    all_map_pins = []
+    for place in places:
+        all_map_pins.extend(create_map_pin_dict(place))        
+    return all_map_pins
