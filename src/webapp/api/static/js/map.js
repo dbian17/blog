a7fc51aead6b9f4465d5db119b383dee2d6d9401
David Bian <davidto1122@gmail.com>
2025-04-22 16:40:43 -0700

    Hardcode map stylesheet

--- src/webapp/api/static/js/map.js
+++ src/webapp/api/static/js/map.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
     container: 'map',
     style:
-        'https://www.overrice.nyc/map-styles/light.json',
+        '/static/map-style.json',
     center: [-73.96919956110607,40.723901332022166],
     zoom: 12
 });
